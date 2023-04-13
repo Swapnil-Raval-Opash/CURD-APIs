8f4ec28ec30fbbe5be71e47bafb0f7c7b7ed3d29
Swapnil Opash <sra.opash@gmail.com>
2023-04-13 09:56:41 +0530

    folder changes

--- backend/routes/contact.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const router = require("express").Router();
-const contact = require("../controllers/contact.controller");
-
-
-router.post("/get-contact", contact.searchContact);
-
-module.exports = router;
